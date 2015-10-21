b758eff17fa4bf4cb6c93cd86cdd7d4b7af7ae30
Omega& <837366964@qq.com>
2015-10-21 18:41:14 +0800

    Create 002.py

---
+++ 002/002.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#a.txt is a file contain the raw text
+
+f = "".join(open('a.txt','r').readlines()).replace('\n','')
+d = {}
+rare = set()
+answer = []
+for i in f:
+    for j in i:
+        d[j]=d.get(j,0)+1
+for i in d:
+    if d[i] < 10:
+        rare.add(i)
+for i in f:
+    if i in rare:
+        answer.append(i)
+print ''.join(answer)
