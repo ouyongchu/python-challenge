9fc731d0be2a5c3c51f2236b62a9398d5caa24a9
Omega& <837366964@qq.com>
2015-10-21 20:02:37 +0800

    Create 01.py

---
+++ main/01.py
@@ -0,0 +1,14 @@
+
+#Caesar cipher
+
+import string
+
+low_letters = sting.lowercase
+table = string.maketrans(low_letters,low_letters[:2]+low_letters[:2])
+
+text = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. 
+bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle.
+sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
+
+print text.translate(table)
+print 'map'.translate(table)
