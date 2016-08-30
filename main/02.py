1f5098b0ec46b9587b4ac05e1f79c3ffe7b57e6c
Omega& <837366964@qq.com>
2016-08-30 13:58:59 +0800

    Update 02.py

--- main/02.py
+++ main/02.py
@@ -3,16 +3,10 @@
 #find the rare characters
 #a.txt is a file containing the raw text
 
-f = "".join(open('a.txt','r').readlines()).replace('\n','')
-d = {}
-rare = set()
-answer = []
-for i in f:
-    d[i]=d.get(i,0)+1
-for i in d:
-    if d[i] < 10:
-        rare.add(i)
-for i in f:
-    if i in rare:
-        answer.append(i)
-print ''.join(answer)
+with open('a.txt','r') as fd:
+    data = ''.join([_.rstrip() for _ in fd])
+    freq = {}
+    for _ in data:
+        freq[_] = freq.get(_,0) + 1
+    avg_freq = len(data) // len(freq)
+    print(''.join([_ for _ in data if freq[_] < avg_freq]))
