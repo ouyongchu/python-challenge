39a7fd9bbfe92eb52f62714de897fd604e7eabd0
omega <yongchu.oyc@alibaba-inc.com>
2016-08-31 17:57:01 +0800

    04.py

---
+++ main/04.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+from urllib.request import urlopen
+
+url_prefix = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
+url = url_prefix + '63579'
+num_pat = re.compile('([0-9]+)')
+
+while True:
+	res = urlopen(url).read().decode('utf-8').strip()
+	print(res)
+	num = num_pat.search(res).group(1)
+	print(num)
+	if not num:
+		break
+	url = url_prefix + num
---
+++ main/url.py
@@ -0,0 +1,2 @@
+4: http://www.pythonchallenge.com/pc/def/linkedlist.php
+5: http://www.pythonchallenge.com/pc/def/peak.html
